0988b0bd56b13dd37451c98746ba514f98683332
pranjali100 <123398720+pranjali100@users.noreply.github.com>
2023-01-26 23:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("head").innerHTML= "Javascript Topic";
+document.getElementById("p1").innerHTML="JavaScript is a powerful, flexible, and fast programming language.";
+document.getElementById("p2").innerHTML="JavaScript powers the dynamic behavior on websites.";
+document.getElementById("p3").innerHTML="JavaScript remains at the core of web development.";
+
+document.body.style.backgroundColor="Blue";
+document.getElementById('head').style.color='red';
+document.getElementById('p1').style.fontSize='30px';
+document.getElementById('p2').style.color='red';
+document.getElementById('p3').style.fontSize='30px';
+document.getElementById('p2').style.fontSize='30px';
+
+
+
+
+
